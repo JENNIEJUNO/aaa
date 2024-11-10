f1d2e90ac865677746dcde86282f12624d3386f7
Jennie Juno <jennie@noreply.com>
2024-11-11 01:08:37 +0900

    새 파일 추가  CSS 자바스크립트 HTM 업데이트

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,12 @@
 import React, { useState } from 'react'
 import styles from './Home.module.css'
 import { useNavigate } from 'react-router-dom'
+import { country } from './Country'
 
 const Home = () => {
     const [text, setText] = useState('집을 소개하는 문장')
     const [goToMenu, setGoToMenu] = useState('메뉴로 이동 ->')
-    const country = {
-        Thailand:{
-            name:"thailand",
-            img:"https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13270987&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMC8yMS9DTFMxMDAwNC8xMzI3MDk4N19XUlRfMjFfQ0xTMTAwMDRfMjAyMDEyMThfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
-            text:"ประโยคแนะนำบ้าน",
-            menuText:"ไปที่เมนู ->",
-        },
-        Vietnam:{
-            name:"vietnam",
-            img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/255px-Flag_of_Vietnam.svg.png",
-            text:"Câu giới thiệu ngôi nhà",
-            menuText:"Đi tới thực đơn ->",
-        },
-        Philippines:{
-            name:"philippines",
-            img:"https://i.namu.wiki/i/iHFjVQ4YlFtJtSJTmjiwH-8U7FNF75Ee_TUSfKDKxc-0mAkEIvBucY6WpxYZ2BQRhZwQOZZ1X2tUviGgVfuv0Q.webp",
-            text:"Pangungusap na nagpapakilala sa bahay",
-            menuText:"Pumunta sa menu ->",
-        }
-    }
+    
     const navigate = useNavigate()
     const goToHomeMenu = () => {
         navigate("/HomeMenu")
---
+++ src/Pages/Workplace/Country.jsx
@@ -0,0 +1,20 @@
+export const country = {
+    Thailand:{
+        name:"thailand",
+        img:"https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13270987&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMC8yMS9DTFMxMDAwNC8xMzI3MDk4N19XUlRfMjFfQ0xTMTAwMDRfMjAyMDEyMThfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
+        text:"ประโยคแนะนำบ้าน",
+        menuText:"ไปที่เมนู ->",
+    },
+    Vietnam:{
+        name:"vietnam",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/255px-Flag_of_Vietnam.svg.png",
+        text:"Câu giới thiệu ngôi nhà",
+        menuText:"Đi tới thực đơn ->",
+    },
+    Philippines:{
+        name:"philippines",
+        img:"https://i.namu.wiki/i/iHFjVQ4YlFtJtSJTmjiwH-8U7FNF75Ee_TUSfKDKxc-0mAkEIvBucY6WpxYZ2BQRhZwQOZZ1X2tUviGgVfuv0Q.webp",
+        text:"Pangungusap na nagpapakilala sa bahay",
+        menuText:"Pumunta sa menu ->",
+    }
+}
