b5b0e9e8811f2d7cd0464a213da4d02216ae9460
Jennie Juno <jennie@noreply.com>
2024-11-15 01:31:28 +0900

    CSS HTML 자바스크립트 업데이트

--- src/Pages/Home/HomeMenu.jsx
+++ src/Pages/Home/HomeMenu.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.css'
 import { menu } from './Object'
 
 const HomeMenu = () => {
+  const [keyword, setKeyword] = useState('')
+  const [filterMenu, setFilterMenu] = useState(menu)
+  const handleChange = (event) => {
+    setKeyword(event.target.value)
+  }
+  const findToKeyword = () => {
+    if(keyword){
+      setFilterMenu(menu.filter(item => item.keyword.includes(keyword)))
+    }
+    else{
+      setFilterMenu(menu)
+    }
+    setKeyword('')
+  }
   return (
     <div className={styles.homeMenu}>
       <div className={styles.homeSearch}>
-        <input type="text" />
-        <button>click</button>
+        <input type="text" placeholder='지역, 월세, 평수 ' value={keyword} onChange={(event) => handleChange(event)}/>
+        <button onClick={findToKeyword}>click</button>
       </div>
         <ul>
-          {menu.map((item) => (
+          {filterMenu.map((item) => (
             <li>
               <img src={item.img} />
               <div>
